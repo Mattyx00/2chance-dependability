03c1603f77727441c939d2cd72a77586de14396b
Aldo Manco <53258029+aldo-manco@users.noreply.github.com>
2026-01-14 18:23:28 +0100

    feat: jml_formal_verification_completely_successful (finally :D)

--- src/main/java/model/beans/WishList.java
+++ src/main/java/model/beans/WishList.java
@@ -3,15 +3,31 @@ package model.beans;
 import java.util.ArrayList;
 
 public class WishList {
-    private /*@ spec_public non_null @*/ Utente utente;
+
+    /*@ spec_public @*/
+    private Utente utente;
+    /*@ spec_public @*/
     private ArrayList<Prodotto> prodotti;
 
+    /*@
+      @ public invariant utente != null;
+      @ public invariant prodotti != null;
+      @*/
 
+    /*@
+      @ ensures utente != null;
+      @ ensures prodotti != null;
+      @*/
     public WishList() {
         this.utente = new Utente();
         prodotti = new ArrayList<>();
     }
 
+    /*@
+      @ requires utente != null;
+      @ ensures this.utente == utente;
+      @ ensures prodotti != null;
+      @*/
     public WishList(Utente utente) {
         if (utente == null) {
             throw new IllegalArgumentException("L'utente non può essere null");
@@ -20,10 +36,18 @@ public class WishList {
         this.prodotti = new ArrayList<>();
     }
 
+    /*@
+      @ ensures \result == utente;
+      @ ensures \result != null;
+      @*/
     public Utente getUtente() {
         return utente;
     }
 
+    /*@
+      @ requires utente != null;
+      @ ensures this.utente == utente;
+      @*/
     public void setUtente(Utente utente) {
         if (utente == null) {
             throw new IllegalArgumentException("L'utente non può essere null");
@@ -31,10 +55,18 @@ public class WishList {
         this.utente = utente;
     }
 
+    /*@
+      @ ensures \result == prodotti;
+      @ ensures \result != null;
+      @*/
     public ArrayList<Prodotto> getProdotti() {
         return prodotti;
     }
 
+    /*@
+      @ requires prodotti != null;
+      @ ensures this.prodotti == prodotti;
+      @*/
     public void setProdotti(ArrayList<Prodotto> prodotti) {
         if (prodotti == null) {
             throw new IllegalArgumentException("La lista dei prodotti non può essere null");
