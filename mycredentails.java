899ce925c03eb7779b3f58c0e7eb9d47c01d7190
Matteo Sacco <matteosacco00@gmail.com>
2026-01-20 20:21:48 +0100

    feat: implementing gitguardian

--- mycredentails.java
+++ mycredentails.java
@@ -1,3 +1,11 @@
 public class mycredentails {
     private static final String GITGUARDIAN_API_KEY = "SCANKANKN";
+
+    public static String getGitGuardianApiKey() {
+        return GITGUARDIAN_API_KEY;
+    }
+
+    public static String getPrivatePassword(){
+        return "passwordSegretaMatteo";
+    }
 }
